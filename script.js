0b6a6785825ae86586a61e07517afe2aa4ec14cd
Tanish Arya <139849099+TanishArya@users.noreply.github.com>
2024-03-24 12:29:24 +0530

    Update 1

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-var inputcalue = document.querySelector('#cityinput')
-var btn = document.querySelector('#add')
-var city = document.querySelector('#cityoutput')
-var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
-apik = "426ff7ffa07feb586d38bd6568168d24"
-function converting(val)
-{
-    return(val - 273).toFixed(3)
-}
+    // Use const for variables that won't be reassigned
+    const apiKey = "426ff7ffa07feb586d38bd6568168d24";
+    const inputValue = document.querySelector('#cityinput');
+    const btn = document.querySelector('#add');
+    const city = document.querySelector('.cityoutput');
+    const description = document.querySelector('#description');
+    const temp = document.querySelector('#temp');
+    const wind = document.querySelector('#wind');
 
-btn.addEventListener('click',function()
-{ 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
-    .then(res => res.json())
+    // Move the function outside of the event listener
+    function convertCelsius(value) {
+      return (value - 273.15).toFixed(2);
+    }
 
-    .then(data =>
-        {
-            var nameval = data['name']
-            var descrip = data['weather']['0']['description']
-            var tempature = data['main']['temp']
-            var wndspeed = data['wind']['speed']
+    // Add event listener to the button
+    btn.addEventListener('click', () => {
+      // Use template literals for string concatenation
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=${apiKey}`;
 
-            city.innerHTML = 'Weather of <span>${nameval}<span>'
-            temp.innerHTML = 'Temperature: <span>${convertion(tempature)}C<span>'
-            description.innerHTML = 'Sky Conditions: <span>${descrip}<span>'
-            wind.innerHTML = 'Wind Speed: <span>${wndspeed} km/h<span>'
-
+      fetch(url)
+        .then(response => {
+          // Check if the response is OK
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
         })
-        .catch(err => alert("You Entered Wrong City Name"))
-}
-)
+        .then(data => {
+          // Use template literals for string concatenation
+          const nameVal = data.name;
+          const descrip = data.weather[0].description;
+          const tempVal = convertCelsius(data.main.temp);
+          const windSpeed = data.wind.speed;
+
+          city.innerHTML = `Weather of <span>${nameVal}</span>`;
+          temp.innerHTML = `Temperature: <span>${tempVal}°C</span>`;
+          description.innerHTML = `Sky Conditions: <span>${descrip}</span>`;
+          wind.innerHTML = `Wind Speed: <span>${windSpeed} m/s</span>`;
+        })
+        .catch(error => {
+          // Log the error to the console
+          console.error('Error:', error);
+          alert("You entered a wrong city name, please try again.");
+        });
+    });
